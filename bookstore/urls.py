2d0653bcf2958e466bdf5ad798ae94c846a07d90
Luiz Francisco <luizfk.marinho@gmail.com>
2025-02-08 16:38:52 -0300

    style: code formatting with black

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,16 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('__debug__/', include(debug_toolbar.urls)),
+    path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
-    re_path(r"^bookstore/(?P<version>(v1|v2))/", include('order.urls')),
-    re_path(r"^bookstore/(?P<version>(v1|v2))/", include('product.urls')),
-    path('api-token-auth/', views.obtain_auth_token, name='api_token_auth')
-
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("api-token-auth/", views.obtain_auth_token, name="api_token_auth"),
 ]
