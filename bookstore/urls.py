e3f7c850700a40efa494040d055188780e3ec1e1
Luiz Francisco <luizfk.marinho@gmail.com>
2025-02-19 17:19:54 -0300

    alterando urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
-from django.urls import path, re_path, include
-from rest_framework.authtoken import views
+from django.urls import include, path, re_path
+
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
@@ -27,4 +28,5 @@ urlpatterns = [
     re_path(r"^bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", views.obtain_auth_token, name="api_token_auth"),
     path("update_server/", views.update, name="update"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
