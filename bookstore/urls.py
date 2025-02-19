084445cedba969246fd524915bcc2648c02154f7
Luiz Francisco <luizfk.marinho@gmail.com>
2025-02-19 17:06:44 -0300

    feat: add views.py alterando urls e settings

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     re_path(r"^bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path(r"^bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", views.obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
 ]
